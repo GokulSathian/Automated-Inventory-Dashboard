06c81f4439cf2e6d3a413caf89ee34177be01bb0
Gokul Sathian <pullarikalgokul@gmail.com>
2023-03-14 23:54:18 -0400

    nav-bar logout

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+# import sys
+# print(sys.path)
+from flask import Flask, render_template, url_for, request, session, redirect, flash
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import UserMixin
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqplite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-db.init_app(app)
-# database = {'Manager1': 'Manager@123'}
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# app.config['SECRET_KEY'] = 'thisisasecretkey'
+# db = SQLAlchemy(app)
+
+# db.init_app(app)
 
 
-class User(db.Model, UserMixin):
+database = {'Manager1': 'Manager@123'}
+app.secret_key = "hello"
+
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), nullable=False, unique=True)
+#     password = db.Column(db.String(80), nullable=False)
 
 
 @app.route('/')
@@ -20,25 +29,35 @@ def home():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    name1 = request.form['username']
+    name = request.form['username']
     pwd = request.form['password']
-    if name1 not in database:
+    if name not in database:
         return render_template('home.html', info='Invalid User')
     else:
-        if database[name1] != pwd:
+        if database[name] != pwd:
             return render_template('home.html', info='Invalid Password')
         else:
-            return render_template('dashboard.html', name=name1)
+            session["name"] = name
+            flash('Login Successfull.')
+            return redirect(url_for("dashboard"))
 
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    # name = session.get['name']
+    return render_template('check.html', username=session["name"])
 
 
-@app.route('/check', methods=['POST', 'GET'])
-def check():
-    return render_template('check.html')
+@app.route('/logout')
+def logout():
+    session.pop("name", None)
+    flash('Logged Out Successfull.')
+    return redirect(url_for("home"))
+
+
+# @app.route('/check', methods=['POST', 'GET'])
+# def check():
+#     return render_template('check.html')
 
 
 if __name__ == '__main__':
